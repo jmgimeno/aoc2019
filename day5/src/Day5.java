b7513da79e5f86c091175ce80ebfeb7678b06b33
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2019-12-05 19:47:07 +0100

    Solved day5

--- day5/src/Day5.java
+++ day5/src/Day5.java
@@ -5,30 +5,12 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day5 {
-
-    enum Operation {ADD, MUL, INPUT, OUTPUT, JUMP_IF_TRUE, JUMP_IF_FALSE, LESS_THAN, EQUALS, HALT}
-
-    enum Mode {POSITION, IMMEDIATE}
-
-    static Map<Integer, Operation> operationDecoder = Map.of(
-            1, Operation.ADD,
-            2, Operation.MUL,
-            3, Operation.INPUT,
-            4, Operation.OUTPUT,
-            5, Operation.JUMP_IF_TRUE,
-            6, Operation.JUMP_IF_FALSE,
-            7, Operation.LESS_THAN,
-            8, Operation.EQUALS,
-            99, Operation.HALT);
-
-    static Map<Integer, Mode> modeDecoder = Map.of(
-            0, Mode.POSITION,
-            1, Mode.IMMEDIATE);
 
     static class Machine {
 
@@ -36,6 +18,24 @@ public class Day5 {
         Iterator<Integer> input;
         List<Integer> output;
         int pc;
+
+        enum Operation {ADD, MUL, INPUT, OUTPUT, JUMP_IF_TRUE, JUMP_IF_FALSE, LESS_THAN, EQUALS, HALT}
+
+        static Map<Integer, Operation> operationDecoder = Map.of(
+                1, Operation.ADD,
+                2, Operation.MUL,
+                3, Operation.INPUT,
+                4, Operation.OUTPUT,
+                5, Operation.JUMP_IF_TRUE,
+                6, Operation.JUMP_IF_FALSE,
+                7, Operation.LESS_THAN,
+                8, Operation.EQUALS,
+                99, Operation.HALT);
+
+        Map<Integer, Function<Integer, Integer>> accessorDecoder = Map.of(
+                0, pos -> state.get(state.get(pos)),
+                1, pos -> state.get(pos)
+        );
 
         Machine(String program) {
             state = Stream.of(program.split(","))
@@ -55,109 +55,76 @@ public class Day5 {
             return output;
         }
 
-        @Override
-        public String toString() {
-            return state.stream().map(Object::toString).collect(Collectors.joining(","));
-        }
-
         class Instruction {
+
             final Operation operation;
-            final Mode[] argModes;
+            final List<Function<Integer, Integer>> accessors;
 
             Instruction(int opcode) {
                 operation = operationDecoder.get(opcode % 100);
-                argModes = IntStream
+                accessors = IntStream
                         .iterate(opcode / 100, i -> i / 10)
-                        .mapToObj(i -> modeDecoder.get(i % 10))
+                        .mapToObj(i -> accessorDecoder.get(i % 10))
                         .limit(3)
-                        .toArray(Mode[]::new);
+                        .collect(Collectors.toUnmodifiableList());
             }
 
             private boolean isHalt() {
                 return operation != Operation.HALT;
             }
 
-            int getPosition(int pos) {
-                return state.get(state.get(pos));
-            }
-
-            void setPosition(int pos, int value) {
+            void set(int pos, int value) {
                 state.set(state.get(pos), value);
             }
 
-            int getImmediate(int pos) { return state.get(pos); }
+            int getArg(int i) {
+                return accessors.get(i - 1).apply(pc + i);
+            }
 
             private void execute() {
                 switch (operation) {
                     case ADD -> {
-                        var arg1 = argModes[0] == Mode.POSITION ? getPosition(pc + 1) : getImmediate(pc + 1);
-                        var arg2 = argModes[1] == Mode.POSITION ? getPosition(pc + 2) : getImmediate(pc + 2);
-                        var result = arg1 + arg2;
-                        assert argModes[2] == Mode.POSITION;
-                        setPosition(pc + 3, result);
+                        set(pc + 3, getArg(1) + getArg(2));
                         pc += 4;
                     }
                     case MUL -> {
-                        var arg1 = argModes[0] == Mode.POSITION ? getPosition(pc + 1) : getImmediate(pc + 1);
-                        var arg2 = argModes[1] == Mode.POSITION ? getPosition(pc + 2) : getImmediate(pc + 2);
-                        var result = arg1 * arg2;
-                        assert argModes[2] == Mode.POSITION;
-                        setPosition(pc + 3, result);
+                        set(pc + 3, getArg(1) * getArg(2));
                         pc += 4;
                     }
                     case INPUT -> {
-                        var value = input.next();
-                        assert argModes[0] == Mode.POSITION;
-                        setPosition(pc + 1, value);
+                        set(pc + 1, input.next());
                         pc += 2;
                     }
                     case OUTPUT -> {
-                        var arg1 = argModes[0] == Mode.POSITION ? getPosition(pc + 1) : getImmediate(pc + 1);
-                        output.add(arg1);
+                        output.add(getArg(1));
                         pc += 2;
                     }
                     case JUMP_IF_TRUE -> {
-                        var arg1 = argModes[0] == Mode.POSITION ? getPosition(pc + 1) : getImmediate(pc + 1);
-                        if (arg1 != 0) {
-                            pc = argModes[1] == Mode.POSITION ? getPosition(pc + 2) : getImmediate(pc + 2);
+                        if (getArg(1) != 0) {
+                            pc = getArg(2);
                         } else {
                             pc += 3;
                         }
                     }
                     case JUMP_IF_FALSE -> {
-                        var arg1 = argModes[0] == Mode.POSITION ? getPosition(pc + 1) : getImmediate(pc + 1);
-                        if (arg1 == 0) {
-                            pc = argModes[1] == Mode.POSITION ? getPosition(pc + 2) : getImmediate(pc + 2);
+                        if (getArg(1) == 0) {
+                            pc = getArg(2);
                         } else {
                             pc += 3;
                         }
                     }
                     case LESS_THAN -> {
-                        var arg1 = argModes[0] == Mode.POSITION ? getPosition(pc + 1) : getImmediate(pc + 1);
-                        var arg2 = argModes[1] == Mode.POSITION ? getPosition(pc + 2) : getImmediate(pc + 2);
-                        var result = arg1 < arg2 ? 1 : 0;
-                        assert argModes[2] == Mode.POSITION;
-                        setPosition(pc + 3, result);
+                        set(pc + 3, getArg(1) < getArg(2) ? 1 : 0);
                         pc += 4;
                     }
-                    case EQUALS -> {
-                        var arg1 = argModes[0] == Mode.POSITION ? getPosition(pc + 1) : getImmediate(pc + 1);
-                        var arg2 = argModes[1] == Mode.POSITION ? getPosition(pc + 2) : getImmediate(pc + 2);
-                        var result = arg1 == arg2 ? 1 : 0;
-                        assert argModes[2] == Mode.POSITION;
-                        setPosition(pc + 3, result);
+                    case EQUALS -> { // I cannot use vars because of Integer equals
+                        set(pc + 3, getArg(1) == getArg(2) ? 1 : 0);
                         pc += 4;
                     }
                     default -> throw new IllegalStateException("Bad op " + operation);
-                };
+                }
             }
         }
-    }
-
-    static String execute(String initial) {
-        var machine = new Machine(initial);
-        machine.run(0);
-        return machine.toString();
     }
 
     static void part1() throws IOException {
--- day5/test/Day5Test.java
+++ day5/test/Day5Test.java
@@ -11,14 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class Day5Test {
 
     @Test
-    void execute() {
-        assertEquals("2,0,0,0,99", Day5.execute("1,0,0,0,99"));
-        assertEquals("2,3,0,6,99", Day5.execute("2,3,0,3,99"));
-        assertEquals("2,4,4,5,99,9801", Day5.execute("2,4,4,5,99,0"));
-        assertEquals("30,1,1,4,2,5,6,0,99", Day5.execute("1,1,1,4,99,5,6,0,99"));
-    }
-
-    @Test
     void part1() throws IOException {
         var program = Files.readString(Paths.get("input.txt")).trim();
         var machine = new Day5.Machine(program);
