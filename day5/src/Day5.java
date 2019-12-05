71ef9a7fb45ecef75924b1eb3fd4295c90191179
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2019-12-05 18:17:57 +0100

    Solved day5 part1 (lots of refactoring to do)

--- day5/src/Day5.java
+++ day5/src/Day5.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,139 +11,127 @@ import java.util.stream.Stream;
 
 public class Day5 {
 
-    enum Operation {ADD, MUL, LOAD, STORE, HALT}
+    enum Operation {ADD, MUL, INPUT, OUTPUT, HALT}
 
     enum Mode {POSITION, IMMEDIATE}
 
     static Map<Integer, Operation> operationDecoder = Map.of(
             1, Operation.ADD,
             2, Operation.MUL,
-            3, Operation.LOAD,
-            4, Operation.STORE,
+            3, Operation.INPUT,
+            4, Operation.OUTPUT,
             99, Operation.HALT);
-
-    static Map<Operation, Integer> operationLength = Map.of(
-            Operation.ADD, 4,
-            Operation.MUL, 4,
-            Operation.LOAD, 2,
-            Operation.STORE, 2,
-            Operation.HALT, 1);
 
     static Map<Integer, Mode> modeDecoder = Map.of(
             0, Mode.POSITION,
             1, Mode.IMMEDIATE);
 
-    static class Instruction {
-        final Operation operation;
-        final Mode[] argModes;
-
-        Instruction(int opcode) {
-            operation = operationDecoder.get(opcode % 100);
-            argModes = IntStream
-                    .iterate(opcode / 100, i -> i / 10)
-                    .mapToObj(i -> modeDecoder.get(i % 10))
-                    .limit(3)
-                    .toArray(Mode[]::new);
-        }
-    }
-
     static class Machine {
 
         List<Integer> state;
+        Iterator<Integer> input;
+        List<Integer> output;
+        int pc;
 
-        Machine(String initial) {
-            state = Stream.of(initial.split(","))
+        Machine(String program) {
+            state = Stream.of(program.split(","))
                     .map(Integer::parseInt)
                     .collect(Collectors.toCollection(ArrayList::new));
         }
 
-        int get(int pos) {
-            return state.get(state.get(pos));
-        }
-
-        void set(int pos, int value) {
-            state.set(state.get(pos), value);
-        }
-
-        void run() {
-            int pc = 0;
+        List<Integer> run(int value) {
+            input = List.of(value).iterator();
+            output = new ArrayList<>();
+            pc = 0;
             Instruction instruction = new Instruction(state.get(pc));
-            while (instruction.operation != Operation.HALT) {
-                var arg1 = get(pc + 1);
-                var arg2 = get(pc + 2);
-                var result = switch (instruction.operation) {
-                    case ADD -> arg1 + arg2;
-                    case MUL -> arg1 * arg2;
-                    default -> throw new IllegalStateException("Bad op " + instruction.operation);
-                };
-                set(pc + 3, result);
-                pc += operationLength.get(instruction.operation);
+            while (instruction.isHalt()) {
+                instruction.execute();
                 instruction = new Instruction(state.get(pc));
             }
-        }
-
-        void restore1202() {
-            setNounAndVerb(12, 2);
-        }
-
-        void setNounAndVerb(int noun, int verb) {
-            state.set(1, noun);
-            state.set(2, verb);
+            return output;
         }
 
         @Override
         public String toString() {
             return state.stream().map(Object::toString).collect(Collectors.joining(","));
         }
+
+        class Instruction {
+            final Operation operation;
+            final Mode[] argModes;
+
+            Instruction(int opcode) {
+                operation = operationDecoder.get(opcode % 100);
+                argModes = IntStream
+                        .iterate(opcode / 100, i -> i / 10)
+                        .mapToObj(i -> modeDecoder.get(i % 10))
+                        .limit(3)
+                        .toArray(Mode[]::new);
+            }
+
+            private boolean isHalt() {
+                return operation != Operation.HALT;
+            }
+
+            int getPosition(int pos) {
+                return state.get(state.get(pos));
+            }
+
+            void setPosition(int pos, int value) {
+                state.set(state.get(pos), value);
+            }
+
+            int getImmediate(int pos) { return state.get(pos); }
+
+            private void execute() {
+                switch (operation) {
+                    case ADD -> {
+                        var arg1 = argModes[0] == Mode.POSITION ? getPosition(pc + 1) : getImmediate(pc + 1);
+                        var arg2 = argModes[1] == Mode.POSITION ? getPosition(pc + 2) : getImmediate(pc + 2);
+                        var result = arg1 + arg2;
+                        assert argModes[2] == Mode.POSITION;
+                        setPosition(pc + 3, result);
+                        pc += 4;
+                    }
+                    case MUL -> {
+                        var arg1 = argModes[0] == Mode.POSITION ? getPosition(pc + 1) : getImmediate(pc + 1);
+                        var arg2 = argModes[1] == Mode.POSITION ? getPosition(pc + 2) : getImmediate(pc + 2);
+                        var result = arg1 * arg2;
+                        assert argModes[2] == Mode.POSITION;
+                        setPosition(pc + 3, result);
+                        pc += 4;
+                    }
+                    case INPUT -> {
+                        var value = input.next();
+                        assert argModes[0] == Mode.POSITION;
+                        setPosition(pc + 1, value);
+                        pc += 2;
+                    }
+                    case OUTPUT -> {
+                        var arg1 = argModes[0] == Mode.POSITION ? getPosition(pc + 1) : getImmediate(pc + 1);
+                        output.add(arg1);
+                        pc += 2;
+                    }
+                    default -> throw new IllegalStateException("Bad op " + operation);
+                };
+            }
+        }
     }
 
     static String execute(String initial) {
         var machine = new Machine(initial);
-        machine.run();
+        machine.run(0);
         return machine.toString();
     }
 
     static void part1() throws IOException {
-        var program = Files.readString(Paths.get("input-day2.txt")).trim();
+        var program = Files.readString(Paths.get("input.txt")).trim();
         var machine = new Machine(program);
-        machine.restore1202();
-        machine.run();
-        System.out.println("part1 = " + machine.state.get(0));
-    }
-
-    static class IntTriple {
-        final int result;
-        final int noun;
-        final int verb;
-
-        public IntTriple(int result, int noun, int verb) {
-            this.result = result;
-            this.noun = noun;
-            this.verb = verb;
-        }
-    }
-
-    static IntTriple run(String program, int noun, int verb) {
-        var machine = new Machine(program);
-        machine.setNounAndVerb(noun, verb);
-        machine.run();
-        return new IntTriple(machine.state.get(0), noun, verb);
-    }
-
-    static void part2() throws IOException {
-        var program = Files.readString(Paths.get("input-day2.txt")).trim();
-        var part2 = IntStream.range(0, 100)
-                .boxed()
-                .flatMap(noun -> IntStream.range(0, 100).mapToObj(verb -> run(program, noun, verb)))
-                .dropWhile(r -> r.result != 19690720)
-                .map(r -> 100 * r.noun + r.verb)
-                .findFirst()
-                .orElseThrow();
-        System.out.printf("part2 = %d%n", part2);
+        var output = machine.run(1);
+        System.out.println("part1 = " + output);
     }
 
     public static void main(String[] args) throws IOException {
         part1();
-        part2();
     }
 }
--- day5/test/Day5Test.java
+++ day5/test/Day5Test.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day5Test {
