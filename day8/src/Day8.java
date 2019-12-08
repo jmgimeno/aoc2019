4fa3d7d6dcb7e0cd0f361a2cb95b0cb8a31a1a19
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2019-12-08 09:48:42 +0100

    Solved day8

--- day8/src/Day8.java
+++ day8/src/Day8.java
@@ -1,7 +1,11 @@
+import javax.sound.midi.SysexMessage;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Day8 {
@@ -9,25 +13,70 @@ public class Day8 {
     static final int WIDE = 25;
     static final int TALL = 6;
 
+    static final int BLACK = 0;
+    static final int WHITE = 1;
+    static final int TRANSPARENT = 2;
+
+    static class Layer {
+        final int[] pixels;
+
+        public Layer(int size) {
+            this.pixels = IntStream
+                    .generate(() -> TRANSPARENT)
+                    .limit(size).toArray();
+        }
+
+        public Layer(int[] pixels) {
+            this.pixels = pixels;
+        }
+
+        public void mergeWith(Layer down) {
+            IntStream.range(0, pixels.length)
+                    .forEach(i -> {
+                        if (pixels[i] == TRANSPARENT)
+                            pixels[i] = down.pixels[i];
+                    });
+        }
+
+        public IntStream asStream() {
+            return Arrays.stream(pixels);
+        }
+
+        public void render(int wide) {
+            String str = asStream().mapToObj(i -> i == BLACK ? " " : "\u25a0")
+                    .collect(Collectors.joining());
+            IntStream.range(0, pixels.length / wide)
+                    .mapToObj(r -> str.substring(r * wide, (r + 1) * wide))
+                    .forEach(System.out::println);
+        }
+    }
+
     static class Image {
         private final int wide;
         private final int tall;
-        private final int[] pixels;
+        private final List<Layer> layers;
 
         Image(int wide, int tall, String str) {
             this.wide = wide;
             this.tall = tall;
-            this.pixels = str.chars().map(c -> c - '0').toArray();
+            this.layers = createLayers(str, wide * tall);
+        }
+
+        private List<Layer> createLayers(String str, int size) {
+            var numLayers = str.length() / size;
+            return IntStream.range(0, numLayers)
+                    .mapToObj(l -> str.substring(l * size, (l + 1) * size))
+                    .map(strLayer -> strLayer.chars().map(c -> c - '0').toArray())
+                    .map(Layer::new)
+                    .collect(Collectors.toUnmodifiableList());
         }
 
         int numLayers() {
-            return pixels.length / (wide * tall);
+            return layers.size();
         }
 
         IntStream getLayer(int numLayer) {
-            var begin = (numLayer - 1) * wide * tall;
-            var end = begin + wide * tall;
-            return IntStream.range(begin, end).map(i -> pixels[i]);
+            return layers.get(numLayer - 1).asStream();
         }
 
         int countValue(int numLayer, int value) {
@@ -37,16 +86,21 @@ public class Day8 {
         }
 
         int layerOfMinZeros() {
-            return IntStream
-                    .rangeClosed(1, numLayers())
+            return IntStream.rangeClosed(1, numLayers())
                     .boxed()
                     .min(Comparator.comparingInt(l -> countValue(l, 0)))
                     .orElseThrow();
         }
+
+        public Layer decode() {
+            var result = new Layer(wide * tall);
+            layers.forEach(result::mergeWith);
+            return result;
+        }
     }
 
     static void part1() throws IOException {
-        var imageString = Files.readString(Paths.get("input.txt"));
+        var imageString = Files.readString(Paths.get("input.txt")).trim();
         var image = new Image(WIDE, TALL, imageString);
         var zeroesLayer = image.layerOfMinZeros();
         var ones = image.countValue(zeroesLayer, 1);
@@ -54,7 +108,16 @@ public class Day8 {
         System.out.println("part1 = " + ones * twos);
     }
 
+    static void part2() throws IOException {
+        var imageString = Files.readString(Paths.get("input.txt")).trim();
+        var image = new Image(WIDE, TALL, imageString);
+        var decoded = image.decode();
+        System.out.println("part2");
+        decoded.render(WIDE);
+    }
+
     public static void main(String[] args) throws IOException {
         part1();
+        part2();
     }
 }
