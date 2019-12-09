d11de404a593a14e44b1469696633bbddd8b8a9b
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2019-12-09 16:06:03 +0100

    Day9 passes tests of day5 after refactoring

--- day9/src/Day9.java
+++ day9/src/Day9.java
@@ -3,149 +3,156 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CountDownLatch;
+import java.util.function.IntUnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-class Memory {
-    final List<Integer> low;
-
-    Memory(List<Integer> program) {
-        this.low = program;
-    }
-
-    int getPosition(int pos) {
-        return low.get(low.get(pos));
-    }
-
-    int getImmediate(int position) {
-        return low.get(position);
-    }
-
-    void set(int pos, int value) {
-        low.set(pos, value);
-    }
-}
-
-class ConcurrentMachine implements Runnable {
-
-    final Memory memory;
-    final BlockingQueue<Integer> qInput;
-    final BlockingQueue<Integer> qOutput;
-    final CountDownLatch endSignal;
-
-    int pc;
-    boolean halted;
-
-    enum Operation {ADD, MUL, INPUT, OUTPUT, JUMP_IF_TRUE, JUMP_IF_FALSE, LESS_THAN, EQUALS, HALT}
-
-    static Map<Integer, Operation> operationDecoder = Map.of(
-            1, Operation.ADD,
-            2, Operation.MUL,
-            3, Operation.INPUT,
-            4, Operation.OUTPUT,
-            5, Operation.JUMP_IF_TRUE,
-            6, Operation.JUMP_IF_FALSE,
-            7, Operation.LESS_THAN,
-            8, Operation.EQUALS,
-            99, Operation.HALT);
-
-    ConcurrentMachine(String program, BlockingQueue<Integer> qInput, BlockingQueue<Integer> qOutput, CountDownLatch endSignal)  {
-        var bytecode = Stream.of(program.split(","))
-                .map(Integer::parseInt)
-                .collect(Collectors.toCollection(ArrayList::new));
-        this.memory = new Memory(bytecode);
-        this.qInput = qInput;
-        this.qOutput = qOutput;
-        this.endSignal = endSignal;
-    }
-
-    @Override
-    public void run() {
-        try {
-            pc = 0;
-            Instruction instruction = new Instruction(memory.getImmediate(pc));
-            while (!halted) {
-                instruction.execute();
-                instruction = new Instruction(memory.getImmediate(pc));
-            }
-            endSignal.countDown();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    class Instruction {
-
-        final Operation operation;
-        final int[] accessors;
-
-        Instruction(int opcode) {
-            operation = operationDecoder.get(opcode % 100);
-            accessors = IntStream
-                    .iterate(opcode / 100, i -> i / 10)
-                    .map(i -> i % 10)
-                    .limit(3)
-                    .toArray();
-        }
-
-        int getArg(int i) {
-            return switch (accessors[i]) {
-                case 0 -> memory.getPosition(pc + i);
-                case 1 -> memory.getImmediate(pc + i);
-                default -> throw new IllegalStateException("Should not happen");
-            };
-        }
-
-        private void execute() throws InterruptedException {
-            switch (operation) {
-                case ADD -> {
-                    memory.set(pc + 3, getArg(1) + getArg(2));
-                    pc += 4;
-                }
-                case MUL -> {
-                    memory.set(pc + 3, getArg(1) * getArg(2));
-                    pc += 4;
-                }
-                case INPUT -> {
-                    var read = qInput.take();
-                    memory.set(pc + 1, read);
-                    pc += 2;
-                }
-                case OUTPUT -> {
-                    var write = getArg(1);
-                    qOutput.put(write);
-                    pc += 2;
-                }
-                case JUMP_IF_TRUE -> {
-                    if (getArg(1) != 0) {
-                        pc = getArg(2);
-                    } else {
-                        pc += 3;
-                    }
-                }
-                case JUMP_IF_FALSE -> {
-                    if (getArg(1) == 0) {
-                        pc = getArg(2);
-                    } else {
-                        pc += 3;
-                    }
-                }
-                case LESS_THAN -> {
-                    memory.set(pc + 3, getArg(1) < getArg(2) ? 1 : 0);
-                    pc += 4;
-                }
-                case EQUALS -> {
-                    memory.set(pc + 3, getArg(1) == getArg(2) ? 1 : 0);
-                    pc += 4;
-                }
-                case HALT -> {
-                    halted = true;
-                }
-            }
-        }
-    }
-}
-
 public class Day9 {
+
+    static class Memory {
+        final List<Integer> low;
+
+        Memory(List<Integer> program) {
+            low = program;
+        }
+
+        int getPosition(int pos) {
+            return low.get(low.get(pos));
+        }
+
+        int getImmediate(int position) {
+            return low.get(position);
+        }
+
+        void set(int pos, int value) {
+            low.set(low.get(pos), value);
+        }
+    }
+
+    static class ConcurrentMachine implements Runnable {
+
+        final Memory memory;
+        final BlockingQueue<Integer> qInput;
+        final BlockingQueue<Integer> qOutput;
+        final CountDownLatch endSignal;
+
+        int pc;
+        boolean halted;
+
+        enum Operation {ADD, MUL, INPUT, OUTPUT, JUMP_IF_TRUE, JUMP_IF_FALSE, LESS_THAN, EQUALS, HALT}
+        enum Mode {POSITION, IMMEDIATE}
+
+        static Map<Integer, Operation> operationDecoder = Map.of(
+                1, Operation.ADD,
+                2, Operation.MUL,
+                3, Operation.INPUT,
+                4, Operation.OUTPUT,
+                5, Operation.JUMP_IF_TRUE,
+                6, Operation.JUMP_IF_FALSE,
+                7, Operation.LESS_THAN,
+                8, Operation.EQUALS,
+                99, Operation.HALT
+        );
+
+        static Map<Integer, Mode> modeDecoder = Map.of(
+                0, Mode.POSITION,
+                1, Mode.IMMEDIATE
+        );
+
+        ConcurrentMachine(String program, BlockingQueue<Integer> qInput, BlockingQueue<Integer> qOutput, CountDownLatch endSignal)  {
+            this.memory = new Memory(
+                    Stream.of(program.split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toCollection(ArrayList::new)));
+            this.qInput = qInput;
+            this.qOutput = qOutput;
+            this.endSignal = endSignal;
+        }
+
+        @Override
+        public void run() {
+            try {
+                pc = 0;
+                Instruction instruction = new Instruction(memory.getImmediate(pc));
+                while (!halted) {
+                    instruction.execute();
+                    instruction = new Instruction(memory.getImmediate(pc));
+                }
+                endSignal.countDown();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        class Instruction {
+
+            final Operation operation;
+            final List<Mode> accessors;
+
+            Instruction(int opcode) {
+                operation = operationDecoder.get(opcode % 100);
+                accessors = IntStream
+                        .iterate(opcode / 100, i -> i / 10)
+                        .mapToObj(i -> modeDecoder.get(i % 10))
+                        .limit(3)
+                        .collect(Collectors.toUnmodifiableList());
+            }
+
+            int getArg(int i) {
+                return switch(accessors.get(i - 1)) {
+                    case POSITION -> memory.getPosition(pc + i);
+                    case IMMEDIATE -> memory.getImmediate(pc + i);
+                };
+            }
+
+            private void execute() throws InterruptedException {
+                switch (operation) {
+                    case ADD -> {
+                        memory.set(pc + 3, getArg(1) + getArg(2));
+                        pc += 4;
+                    }
+                    case MUL -> {
+                        memory.set(pc + 3, getArg(1) * getArg(2));
+                        pc += 4;
+                    }
+                    case INPUT -> {
+                        var read = qInput.take();
+                        memory.set(pc + 1, read);
+                        pc += 2;
+                    }
+                    case OUTPUT -> {
+                        var write = getArg(1);
+                        qOutput.put(write);
+                        pc += 2;
+                    }
+                    case JUMP_IF_TRUE -> {
+                        if (getArg(1) != 0) {
+                            pc = getArg(2);
+                        } else {
+                            pc += 3;
+                        }
+                    }
+                    case JUMP_IF_FALSE -> {
+                        if (getArg(1) == 0) {
+                            pc = getArg(2);
+                        } else {
+                            pc += 3;
+                        }
+                    }
+                    case LESS_THAN -> {
+                        memory.set(pc + 3, getArg(1) < getArg(2) ? 1 : 0);
+                        pc += 4;
+                    }
+                    case EQUALS -> {
+                        memory.set(pc + 3, getArg(1) == getArg(2) ? 1 : 0);
+                        pc += 4;
+                    }
+                    case HALT -> {
+                        halted = true;
+                    }
+                }
+            }
+        }
+    }
 }
--- day9/test/Day9Test.java
+++ day9/test/Day9Test.java
@@ -1,5 +1,40 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day9Test {
 
+    @Test
+    void day5Part1() throws IOException, InterruptedException {
+        var program = Files.readString(Paths.get("input-day5.txt")).trim();
+        var input = new LinkedBlockingQueue<Integer>();
+        var output = new LinkedBlockingQueue<Integer>();
+        var counter = new CountDownLatch(1);
+        input.put(1);
+        var machine = new Day9.ConcurrentMachine(program, input, output, counter);
+        machine.run();
+        var queueAsList = output.stream().collect(Collectors.toUnmodifiableList());
+        assertEquals(List.of(0, 0, 0, 0, 0, 0, 0, 0, 0, 12234644), queueAsList);
+    }
+
+    @Test
+    void day5Part2() throws IOException, InterruptedException {
+        var program = Files.readString(Paths.get("input-day5.txt")).trim();
+        var input = new LinkedBlockingQueue<Integer>();
+        var output = new LinkedBlockingQueue<Integer>();
+        var counter = new CountDownLatch(1);
+        input.put(5);
+        var machine = new Day9.ConcurrentMachine(program, input, output, counter);
+        machine.run();
+        var queueAsList = output.stream().collect(Collectors.toUnmodifiableList());
+        assertEquals(List.of(3508186), queueAsList);
+    }
 }
